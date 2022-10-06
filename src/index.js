fb75b626db0b94f539b8e6293a3469a0c6a446ae
Manu Calvo <manucalvom@usal.es>
2022-10-06 20:41:28 +0200

    Initial Commit
    Webpack and Firebase starting configuration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKZmXaTxjQ8H5obf6dz7PlhdiqC9Hz898",
+    authDomain: "demeterlab.firebaseapp.com",
+    projectId: "demeterlab",
+    storageBucket: "demeterlab.appspot.com",
+    messagingSenderId: "1054593608090",
+    appId: "1:1054593608090:web:df1458a27f94763ca8f223"
+  };
+
+  const app = initializeApp(firebaseConfig);
