42e94527aec07f0bdd40287b17f40a15ef6aa51b
Manu Calvo <manucalvom@usal.es>
2022-10-07 01:28:21 +0200

    Code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-    watch: true
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  watch: true,
 };
